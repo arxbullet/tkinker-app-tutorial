30f688851dc343a437bf3057c6d74990e6754c63
arx_bullet <aranzav.c.b@gmail.com>
2022-02-06 12:27:15 -0300

    como cargar imagenes tkinker, pillow

--- 03-imagenes.py
+++ 03-imagenes.py
@@ -1,15 +1,19 @@
+from email.mime import image
 from tkinter import *
+from PIL import Image, ImageTk
 
 ventana = Tk()
 ventana.geometry('500x500')
 
 #cargar una imagen dentro de la ventana, para esto usaremos un modulo llamado pillow
-#para eso ejecutaremos pip install --upgrade pillow
-
-
+#para eso ejecutaremos py -m pip install --upgrade pillow
 
 Label(ventana, text='hola, esto es una imagen').pack()
 
+imagen = Image.open('./imagenes/piton.ico')
 
+render = ImageTk.PhotoImage(imagen)
+
+Label(ventana, image = render).pack() #renderizar imagen en un label dentro de la ventana
 
 ventana.mainloop() 
