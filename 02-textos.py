7e63646507bd29a49bbc262de710f9fc9bdac124
arx_bullet <aranzav.c.b@gmail.com>
2022-02-05 23:53:16 -0300

    añadidos keyword arguments

--- 02-textos.py
+++ 02-textos.py
@@ -5,6 +5,17 @@ ventana.geometry('500x500')
 
 texto = Label(ventana, text = 'bienvenido a mi programa')
 
+#keyword arguments 
+
+def pruebas(nombre, apellidos):
+    return f'hola {nombre}{apellidos}'
+
+nuevoTexto = Label(ventana, text=pruebas(apellidos='carrillo',nombre='vale'))
+#aca lo importante o los keyword aguments son apellido y nombre, y al pasarlos 
+#con el nombre del parametro, da lo mismo el orden.
+
+nuevoTexto.pack()
+
 #pasar colores y otras configuraciones de estilo
 texto.config(
     fg ='white',
@@ -17,7 +28,6 @@ texto.config(
 
 #para mostrarlo debo empaquetarlo y ejecutarlo 
 texto.pack()
-
 
 texto = Label(ventana, text = 'soy vale, sigueme para mas contenido')
 texto.pack(anchor=CENTER)#el movimiento del texto se lo tenemos que indicar dentro de pack
