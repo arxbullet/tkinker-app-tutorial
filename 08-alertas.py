410d8688933e0ca3506bbb197ee27d43439b4d86
arx_bullet <aranzav.c.b@gmail.com>
2022-02-06 15:03:09 -0300

    arreglar parametros , añadir texto central

--- 08-alertas.py
+++ 08-alertas.py
@@ -10,23 +10,32 @@ ventana.config(
 )
 
 def MostrarAlertaError():
-    MessageBox.showerror('alerta , ha ocurrido un error')
+    MessageBox.showerror('error','alerta , ha ocurrido un error')
 
 
 Button(ventana, text='mostrar alerta', command=MostrarAlertaError).pack(anchor=NW)
 
 def MostrarAlertaSalir():
-   resultado = MessageBox.askquestion('seguro que quieres salir ?')
+   resultado = MessageBox.askquestion('espera ', 'seguro que quieres salir ?')
    if resultado != 'yes' :
        ventana.destroy()
 
 
-Button(ventana, text='salir', command=MostrarAlertaSalir).pack(anchor=NW)
+Button(ventana, text='salir ahora', command=MostrarAlertaSalir).pack(anchor=NW)
 
 def MostrarAlertainfo():
-    MessageBox.showinfo('esto es una alerta')
+    MessageBox.showinfo('esto es una alerta', 'alerta')
 
 
 Button(ventana, text='mostrar info', command=MostrarAlertainfo).pack(anchor=NW)
+
+def Salir(nombre):
+   resultado = MessageBox.askquestion('seguro que quieres salir ?', 'espera un segundo')
+   if resultado != 'yes' :
+       MessageBox.showinfo('Chao', f'nos vemos {nombre}')
+       ventana.destroy()
+
+
+Button(ventana, text='salir', command=lambda:Salir('vale')).pack(anchor=NW)
 
 ventana.mainloop()
