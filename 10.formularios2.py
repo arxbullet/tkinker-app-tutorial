cd2938370efda45bc23e2f1c37417479be1109bd
arx_bullet <aranzav.c.b@gmail.com>
2022-02-07 12:09:25 -0300

    select buttons y orden de codigo

--- 10.formularios2.py
+++ 10.formularios2.py
@@ -1,4 +1,3 @@
-from distutils import command
 from tkinter import *
 from tkinter import messagebox  as MessageBox
 
@@ -9,6 +8,15 @@ ventana.geometry('1200x800')
 ventana.config(
     bd= 70
 )
+
+#VARIABLES
+
+web=IntVar()
+mobile=IntVar()
+opcion = StringVar()
+opcion.set(None)
+opcionMenu = StringVar()
+opcionMenu.set(None)
 
 #FUNCIONES
 
@@ -32,16 +40,8 @@ def MostrarProfesion():
 def Marcar():
     marcado.config(text=opcion.get())
 
-"""marco = Frame(ventana, width=340, height=200)
-marco.config(
-    padx=15, 
-    pady=15, 
-    bd= 3, 
-    relief=SOLID
-)
-
-marco.pack(side=TOP, anchor=CENTER)
-marco.pack_propagate(False)"""
+def Seleccionar():
+    seleccionado.config(text=opcionMenu.get())
 
 encabezado = Label(ventana, text='TITULOS, CHECKBOX, RADIOBUTTONS Y OTROS')
 encabezado.config(
@@ -55,44 +55,51 @@ encabezado.grid(row=0, column=0, columnspan=5, sticky=W)
 
 #checkbox
 
-web=IntVar()
-mobile=IntVar()
-
 Label(ventana, text='A que te dedicas?').grid(row=1, column=0)
 Checkbutton(
     ventana, 
     text='desarrollo web', 
     variable=web, 
     onvalue=1, 
+    command = MostrarProfesion,
     offvalue=0).grid(row=1, column=1)
 Checkbutton(
     ventana, 
     text='desarrollo movil', 
     variable=mobile, 
     onvalue=1, 
+    command = MostrarProfesion,
     offvalue=0).grid(row=1, column=2)
 
-mostrar = Label(ventana, command=MostrarProfesion)
-mostrar.grid(row=2, column=0)
+mostrar = Label(ventana)
+mostrar.grid(row=1, column=4)
 
 #radio buttons
-opcion = IntVar()
-opcion.set(None)
+
 Label(ventana, text='cual es tu sexo').grid(row=2, column=0)
 Radiobutton(
     ventana, 
     text='femenino', 
     variable=opcion,
     value='femenino',
-    command=Marcar).grid(row=2, column=1)
+    command = Marcar).grid(row=2, column=1)
 Radiobutton(
     ventana, 
     text='Masculino', 
     variable=opcion,
     value='m',
-    command=Marcar).grid(row=2, column=2)
+    command = Marcar).grid(row=2, column=2)
 
 marcado = Label(ventana)
-marcado.grid(row=3, column=0)
+marcado.grid(row=2, column=4)
 
+#Option Menu
+Label(ventana, text='escoje una opcion').grid(row=3, column=0)
+select = OptionMenu(ventana, opcionMenu, 'opcion 1', 'opcion 2', 'opcion 3' )
+select.grid(row=3, column=1)
+seleccionado = Label(ventana)
+seleccionado.grid(row=3, column=3)
+Button(ventana, text='ver',command=Seleccionar).grid(row=3, column=2)
 ventana.mainloop()
+
+
